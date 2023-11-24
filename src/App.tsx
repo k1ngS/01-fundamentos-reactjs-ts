7ef31545e5c72099633a427e612147388a3084e8
k1ngS <totonhorock@gmail.com>
2023-11-24 17:13:40 -0300

    Add: Typescript in components and jsx files.

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+import { Post } from "./components/Post/Post";
+import { Header } from "./components/Header/Header";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+
+import styles from './App.module.css';
+import "./global.css"
+
+// author: { avatar_url: "", name: "", role: "" }
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/k1ngS.png",
+      name: "Marcos Beltrão",
+      role: "Web Developer"
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋',
+      },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare',
+      },
+    ],
+    publishedAt: new Date('2023-11-23 15:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://images.unsplash.com/photo-1647610229306-3906b8f72539?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "André Luis",
+      role: "Black Hat"
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋',
+      },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare',
+      },
+    ],
+    publishedAt: new Date('2023-11-20 15:00:00')
+  },
+];
+
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id} 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
